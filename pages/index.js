14103e01acdfe5203d83171fcf8d03e8ebf21a64
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-09-27 14:50:53 +0530

    Add

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">AtomX Tech</a>
         </h1>
 
+
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        Delivering affordable tech for early stage startups
+          {/* <code className={styles.code}>pages/index.js</code> */}
         </p>
 
         <div className={styles.grid}>
